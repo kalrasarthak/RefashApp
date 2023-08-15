fc27c14e6c7174a6b6faa082705c3655945977b6
Sarthakkalra-26 <sarthak.kalra024@gmail.com>
2023-08-15 12:21:51 +0530

    updated homepage

---
+++ build/precache-manifest.104dcd3adb8b7866e7100c71b53368ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a5145f89548881bb19d",
+    "url": "/kalrasarthak/RefashApp.git/static/js/main.7a5145f8.chunk.js"
+  },
+  {
+    "revision": "2f7e84cdda5cb3164d79",
+    "url": "/kalrasarthak/RefashApp.git/static/js/1.2f7e84cd.chunk.js"
+  },
+  {
+    "revision": "dcb55e773a5c74ca6026",
+    "url": "/kalrasarthak/RefashApp.git/static/js/runtime~main.dcb55e77.js"
+  },
+  {
+    "revision": "1860e27fb05b21bfcc1e6969f681121a",
+    "url": "/kalrasarthak/RefashApp.git/static/media/2.1860e27f.png"
+  },
+  {
+    "revision": "f01a210b77b7fb4d55c8b4531db54807",
+    "url": "/kalrasarthak/RefashApp.git/static/media/4.f01a210b.png"
+  },
+  {
+    "revision": "35c73ff599aed09a2968d3c539ee0117",
+    "url": "/kalrasarthak/RefashApp.git/static/media/7.35c73ff5.png"
+  },
+  {
+    "revision": "33b2253799e6c3bb4a6c1743660e19f9",
+    "url": "/kalrasarthak/RefashApp.git/index.html"
+  }
+];
