13dea2902d6aff614f7f3686324198751452edaa
Sarthakkalra-26 <sarthak.kalra024@gmail.com>
2023-08-15 11:53:42 +0530

    Updates

---
+++ precache-manifest.228bb8adb5a887ec9237b78561ab0aaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a5145f89548881bb19d",
+    "url": "/static/js/main.7a5145f8.chunk.js"
+  },
+  {
+    "revision": "04d8ac04a3408f687cd3",
+    "url": "/static/js/1.04d8ac04.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1860e27fb05b21bfcc1e6969f681121a",
+    "url": "/static/media/2.1860e27f.png"
+  },
+  {
+    "revision": "f01a210b77b7fb4d55c8b4531db54807",
+    "url": "/static/media/4.f01a210b.png"
+  },
+  {
+    "revision": "35c73ff599aed09a2968d3c539ee0117",
+    "url": "/static/media/7.35c73ff5.png"
+  },
+  {
+    "revision": "c6723aac117917a639cd36a4b37071f6",
+    "url": "/index.html"
+  }
+];
