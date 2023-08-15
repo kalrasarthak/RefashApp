3d2e44b97f1d273c19aad6cc75a49856ba031198
Sarthakkalra-26 <sarthak.kalra024@gmail.com>
2023-08-15 12:17:58 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.228bb8adb5a887ec9237b78561ab0aaa.js"
+  "/kalrasarthak/RefashApp.git/precache-manifest.104dcd3adb8b7866e7100c71b53368ba.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/kalrasarthak/RefashApp.git/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
